3380c3e2033492b3a901a28ec8148824cc059361
JoniRont <57128169+JoniRont@users.noreply.github.com>
2020-02-04 21:30:46 +0200

    Initial commit

---
+++ HissiSimulaatio/IHissi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace HissiSimulaatio
+{
+    interface IHissi
+    {
+        void Siirry(int syote);
+        
+        
+        
+        
+    }
+}
---
+++ HissiSimulaatio/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using static System.Console;
+
+namespace HissiSimulaatio
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int haluttukerros;
+            Hissi hissi;
+            hissi = new Hissi(minimi: -2, maksimi: PyydaKokonaisLuku("anna maksimikerros hissille: "));
+            while (true)
+            {
+                
+                haluttukerros = PyydaKokonaisLuku("anna kerros(-3 lopettaa): ");
+                if (haluttukerros == -3)
+                {
+                    break;
+                }
+                hissi.NaytaKerrokset();
+                hissi.Siirry(haluttukerros);
+                
+            }
+
+            //itse ohjelma
+           
+            // tarkistetaan syötteen oikeellisuus
+            static int PyydaKokonaisLuku(string kehote)
+            {
+                int paluu;
+                Write($"{kehote}");
+                do
+                {
+                    try
+                    {
+                        paluu = int.Parse(ReadLine());
+                        break;
+                    }
+                    catch
+                    {
+                        WriteLine("Syöte ei ole kokonaisluku.");
+                    }
+                }
+
+                while (true);
+                return paluu;
+
+            }
+        }
+    }
+}
